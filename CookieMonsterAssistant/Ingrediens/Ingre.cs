f6c425ebea5161f1befd5f69b5b503976de2a82c
AjajajOnt <90178964+AjajajOnt@users.noreply.github.com>
2022-03-04 11:37:19 +0100

    Add project files.

---
+++ CookieMonsterAssistant/Ingrediens/Ingre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieMonsterAssistant.Ingrediens
+{
+    internal class Ingre
+    {
+
+        public int Amount { get; set; }
+        public int Measure { get; set; }
+        public string Ingredient { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
