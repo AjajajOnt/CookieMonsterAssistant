e553fb6b96e2d848b9d49bfc7aab721bc3a31223
AjajajOnt <90178964+AjajajOnt@users.noreply.github.com>
2022-03-10 13:51:36 +0100

    Tester Klara

--- CookieMonsterAssistantTests/Ingrediens/IngreTests.cs
+++ CookieMonsterAssistantTests/Ingrediens/IngreTests.cs
@@ -43,45 +43,47 @@ namespace CookieMonsterAssistant.Ingrediens.Tests
         [TestMethod()]
         public void CupsToDeciTest()
         {
-            string columns = "cups";
-            if (columns.Contains("cups") || columns.Contains("cup"))
-            {
-                columns = "DL";
-            }
-            Assert.AreEqual("DL", columns);
+
+
+            string[] columns = new string[3];
+            columns[1] = "cups";
+            Ingre.CupsToDeci(columns);
+            Assert.AreEqual("DL", columns[1]);
+
         }
 
         [TestMethod()]
         public void ThreeFourthsTest()
         {
-            string columns = "3/4";
-            if (columns.Contains("3/4"))
-            {
-                columns = "0,75";
-            }
-            Assert.AreEqual("0,75", columns);
+
+
+            string[] columns = new string[3];
+            columns[0] = "3/4";
+            Ingre.ThreeFourths(columns);
+            Assert.AreEqual("0,75", columns[0]);
+
         }
 
         [TestMethod()]
         public void OneAndaHalfTest()
         {
-            string columns = "1 1/2";
-            if (columns.Contains("1 1/2"))
-            {
-                columns = "1,50";
-            }
-            Assert.AreEqual("1,50", columns);
+
+
+            string[] columns = new string[3];
+            columns[0] = "1 1/2";
+            Ingre.OneAndaHalf(columns);
+            Assert.AreEqual("1,50", columns[0]);
         }
 
         [TestMethod()]
         public void TwoAndTwoThirdsTest()
         {
-            string columns = "2 2/3";
-            if (columns.Contains("2 2/3"))
-            {
-                columns = "2,66";
-            }
-            Assert.AreEqual("2,66", columns);
+
+
+            string[] columns = new string[3];
+            columns[0] = "2 2/3";
+            Ingre.TwoAndTwoThirds(columns);
+            Assert.AreEqual("2,66", columns[0]);
         }
     }
 }
